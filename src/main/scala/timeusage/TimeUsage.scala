c320800a32d1bef4d1e4c794b2f61928b30f5003
Mounika Koppole <113781162+MounikaKoppole@users.noreply.github.com>
2022-12-06 19:29:48 -0500

    remove activity trait (#2)

--- src/main/scala/timeusage/TimeUsage.scala
+++ src/main/scala/timeusage/TimeUsage.scala
@@ -6,6 +6,23 @@ import org.apache.spark.sql.expressions.Aggregator
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, _}
 import java.io.File
+import scala.util.matching.Regex
+
+sealed trait Activity {
+  val Regex:Regex
+  def unapply(arg: String): Option[String] =  Regex.findFirstIn(arg).map(_ => arg)
+}
+
+object Primary extends Activity {
+  val Regex = "^t(01|03|11|1801|1803)".r
+}
+object Working extends Activity {
+  val Regex = "^t(05|1805)".r
+}
+
+object Leisure extends Activity {
+  val Regex = "^t([01][2467890])".r
+}
 
 /** Main class */
 object TimeUsage {
